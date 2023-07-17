87a860b59537c7105dc96b637ec64466aa869882
Daniel Bílek <daniel.bilek@regiojet.com>
2023-07-17 12:01:26 +0200

    deleted notistack

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,17 +1,16 @@
-import React, { FC } from "react";
-import Container from "../layouts/Container";
+import React, { FC } from 'react';
+import Container from '../layouts/Container';
 
 const Navbar: FC = () => {
-	return (
-		<nav className="p-10 bg-neutral-dark">
-			<Container>
-				<div className="flex items-center justify-center">
-				<h1 className="font-light">Yahtzee</h1>
-				</div>
-				
-			</Container>
-		</nav>
-	);
+  return (
+    <nav className="p-10 bg-neutral-dark">
+      <Container>
+        <div className="flex items-center justify-center">
+          <h1 className="font-light">Yahtzee</h1>
+        </div>
+      </Container>
+    </nav>
+  );
 };
 
 export default Navbar;
